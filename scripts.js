2082f2055254d42cdc64f274527fa5758d4295a8
Andrei Trunkin <inboxavt@gmail.com>
2022-11-12 19:53:24 +0100

    Test. I changed a file and commit it

--- scripts.js
+++ scripts.js
@@ -2,3 +2,6 @@ const showText = () => {
     const text = 'Hello! Glad to see you!\nAre you happy?';
     alert(text); 
 };
+
+//i would like to test Git's work
+//So i am changing this file and go to Git for commit it
